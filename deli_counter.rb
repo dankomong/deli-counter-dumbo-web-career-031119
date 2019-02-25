02bce10a8e9e2180d69bec4ef43354439fbcb853
Daniel Ko <dankomong@gmail.com>
2019-02-25 00:14:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 def line(array)
   if (array.length == 0)
-    "The line is currently empty."
+    puts "The line is currently empty."
   else
     phrase = "The line is currently: "
     array.each_with_index do |name, index|
