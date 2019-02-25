c1ba8deaa2998220603c4d33b223608178feef38
Daniel Ko <dankomong@gmail.com>
2019-02-25 00:20:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,9 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else
-    phrase = "The line is currently: "
+    phrase = "The line is currently:"
     array.each_with_index do |name, index|
-      num_person = "#{index + 1}. #{name}"
+      num_person = " #{index + 1}. #{name}"
       phrase += num_person
     end
     puts phrase
