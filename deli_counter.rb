c945a9915bd17bb3cae3988aa3e56bc250335e3c
Daniel Ko <dankomong@gmail.com>
2019-02-25 00:17:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,26 @@
 # Write your code here.
-def line(array)
-  if (array.length == 0)
+# def line(array)
+#   if (array.length == 0)
+#     puts "The line is currently empty."
+#   else
+#     phrase = "The line is currently: "
+#     array.each_with_index do |name, index|
+#       num_person = "#{index + 1}. #{name}"
+#       phrase += num_person
+#     end
+#   end
+#   puts phrase
+# end
+
+def line(array) 
+  if array.length == 0 
     puts "The line is currently empty."
-  else
-    phrase = "The line is currently: "
-    array.each_with_index do |name, index|
-      num_person = "#{index + 1}. #{name}"
-      phrase += num_person
-    end
-  end
-  phrase
-end
+  else 
+    message = "The line is currently:"
+    
+  array.each_with_index do |value, index| 
+    message += " #{index.to_i+1}. #{value}"
+  end 
+  puts "#{message}"
+  end 
+end 
